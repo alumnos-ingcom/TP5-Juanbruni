8771433299dc1e3727ee1be810b4b9340ef6fdcc
Bruni <juanbruni@live.com>
2021-06-20 20:05:11 -0300

    Create tp5-ej1.py

---
+++ tp5-ej1.py
@@ -0,0 +1,11 @@
+################
+# Juan Ignacio Bruni - @Juanbruni
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def :
+
+    
+if __name__ == "__main__":
+    prueba()
